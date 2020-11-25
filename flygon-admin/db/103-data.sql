4ddca2533d77daab79090250e71fcbce439bdc63
cj <power4j@outlook.com>
2020-11-25 15:15:29 +0800

    update sql file

---
+++ flygon-admin/db/103-data.sql
@@ -0,0 +1,3 @@
+-- sys user
+INSERT INTO `t_sys_user`(`id`, `del_flag`, `create_by`, `create_at`, `update_by`, `update_at`, `username`, `password`, `salt`, `name`, `mail`, `mobile_phone`, `remarks`, `status`)
+VALUES (1330172506892759042, 0, NULL, '2020-10-29 15:51:39', NULL, NULL, 'admin', '$2a$10$OO/CElRgmt9nhFQsHDDs3ulWkTcZ5/4t09YVMVU9Y8AzDthkm6lWO', NULL, '管理员', null, '18081020301', '初始用户', 0);
