4075e84d0303d22fa07be1a67f73eb0c4b5e0129
cj <power4j@outlook.com>
2021-01-18 01:29:37 +0800

    chore(tools): docker deploy script

---
+++ docker-deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "Build and push? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+
+docker build -t registry.cn-hangzhou.aliyuncs.com/power4j/ji-admin-db ./db
+docker push registry.cn-hangzhou.aliyuncs.com/power4j/ji-admin-db
+docker build -t registry.cn-hangzhou.aliyuncs.com/power4j/ji-admin-server ./ji-admin
+docker push registry.cn-hangzhou.aliyuncs.com/power4j/ji-admin-server
